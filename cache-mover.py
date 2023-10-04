c57a1e36d349259b910beab31b876f90fcd10605
muffin <MonsterMuffin@users.noreply.github.com>
2023-10-04 20:30:44 +0100

    merged concurren file move

--- cache-mover.py
+++ cache-mover.py
@@ -1,9 +1,9 @@
 import os
 import shutil
 import logging
-from discord_webhook import DiscordWebhook
-import configparser
 import yaml
+from concurrent.futures import ThreadPoolExecutor
+from discord_webhook import DiscordWebhook
 
 # Load configurations from config.yml
 with open('config.yml', 'r') as config_file:
@@ -13,56 +13,56 @@ CACHE_PATH = config['Paths']['CACHE_PATH']
 BACKING_PATH = config['Paths']['BACKING_PATH']
 LOG_PATH = config['Paths']['LOG_PATH']
 THRESHOLD_PERCENTAGE = config['Settings']['THRESHOLD_PERCENTAGE']
+MAX_WORKERS = config['Settings']['MAX_WORKERS']
 USE_WEBHOOK = config['Webhook']['USE_WEBHOOK']
 WEBHOOK_URL = config['Webhook']['WEBHOOK_URL']
-MAX_WORKERS = config['Settings']['MAX_WORKERS']
-
-# Load configurations from settings.ini
-config = configparser.ConfigParser()
-config.read('settings.ini')
-
-CACHE_PATH = config.get('Paths', 'CACHE_PATH')
-BACKING_PATH = config.get('Paths', 'BACKING_PATH')
-THRESHOLD_PERCENTAGE = config.getint('Settings', 'THRESHOLD_PERCENTAGE')
-USE_WEBHOOK = config.getboolean('Webhook', 'USE_WEBHOOK')
-WEBHOOK_URL = config.get('Webhook', 'WEBHOOK_URL')
 
 # Set up logging
 logging.basicConfig(filename=LOG_PATH, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def get_cache_usage(path):
-    """Get the usage percentage of the filesystem."""
-    stat = os.statvfs(path)
-    return (stat.f_blocks - stat.f_bfree) / stat.f_blocks * 100
+def get_fs_usage(path):
+    """Get filesystem usage percentage."""
+    total, used, free = shutil.disk_usage(path)
+    return (used / total) * 100
 
-def find_oldest_file(path):
-    """Find the oldest file in the given directory."""
-    files = [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
-    return min(files, key=os.path.getctime)
+def gather_files_to_move():
+    """Gather files to be moved until the threshold is no longer exceeded."""
+    files_to_move = []
+    while get_fs_usage(CACHE_PATH) > THRESHOLD_PERCENTAGE:
+        oldest_file = min(
+            (os.path.join(dirname, filename)
+            for dirname, _, filenames in os.walk(CACHE_PATH)
+            for filename in filenames),
+            key=lambda fn: os.stat(fn).st_mtime)
+        files_to_move.append(oldest_file)
+    return files_to_move
 
-def send_webhook_notification(message):
-    """Send a notification using a webhook."""
-    if not USE_WEBHOOK:
-        return
-
-    webhook = DiscordWebhook(url=WEBHOOK_URL, content=message, rate_limit_retry=True)
+def move_file(src, dest):
+    """Move a file and log the action."""
     try:
-        response = webhook.execute()
+        shutil.move(src, dest)
+        logging.info(f"Moved {src} to {dest}")
+        if USE_WEBHOOK:
+            webhook = DiscordWebhook(url=WEBHOOK_URL, content=f"Moved {src} to {dest}")
+            webhook.execute()
     except Exception as e:
-        logging.error(f"Failed to send webhook notification: {e}")
+        logging.error(f"Error moving file: {e}")
+
+def move_files_concurrently(files_to_move):
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        futures = [executor.submit(move_file, src, os.path.join(BACKING_PATH, os.path.basename(src))) for src in files_to_move]
+        for future in futures:
+            try:
+                future.result()
+            except Exception as e:
+                logging.error(f"Error moving file: {e}")
 
 def main():
-    usage = get_cache_usage(CACHE_PATH)
-    if usage > THRESHOLD_PERCENTAGE:
-        logging.info(f"Cache usage is {usage:.2f}%, exceeding threshold. Starting file move...")
-        oldest_file = find_oldest_file(CACHE_PATH)
-        try:
-            shutil.move(oldest_file, BACKING_PATH)
-            logging.info(f"Moved {oldest_file} to {BACKING_PATH}")
-            send_webhook_notification(f"Successfully moved {oldest_file} to {BACKING_PATH}")
-        except Exception as e:
-            logging.error(f"Error moving file: {e}")
-            send_webhook_notification(f"Failed to move {oldest_file} to {BACKING_PATH}. Error: {e}")
+    current_usage = get_fs_usage(CACHE_PATH)
+    if current_usage > THRESHOLD_PERCENTAGE:
+        logging.info(f"Cache usage is {current_usage:.2f}%, exceeding threshold. Starting file move...")
+        files_to_move = gather_files_to_move()
+        move_files_concurrently(files_to_move)
 
 if __name__ == "__main__":
     main()
