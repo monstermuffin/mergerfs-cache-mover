1c0f2cb63e8673367c295a3848be78505ad26f69
muffin <MonsterMuffin@users.noreply.github.com>
2023-10-15 22:13:39 +0100

    add log rotation

--- cache-mover.py
+++ cache-mover.py
@@ -3,6 +3,7 @@ import shutil
 import logging
 import yaml
 from concurrent.futures import ThreadPoolExecutor
+from logging.handlers import RotatingFileHandler
 
 # Load configurations from config.yml
 with open('config.yml', 'r') as config_file:
@@ -13,9 +14,19 @@ BACKING_PATH = config['Paths']['BACKING_PATH']
 LOG_PATH = config['Paths']['LOG_PATH']
 THRESHOLD_PERCENTAGE = config['Settings']['THRESHOLD_PERCENTAGE']
 MAX_WORKERS = config['Settings']['MAX_WORKERS']
+MAX_LOG_SIZE_MB = config['Settings']['MAX_LOG_SIZE_MB']
+BACKUP_COUNT = config['Settings']['BACKUP_COUNT']
 
-# Set up logging
-logging.basicConfig(filename=LOG_PATH, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+# Convert log size from MB to bytes
+MAX_LOG_SIZE_BYTES = MAX_LOG_SIZE_MB * 1024 * 1024
+
+# Set up logging with rotation
+log_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+log_handler = RotatingFileHandler(LOG_PATH, maxBytes=MAX_LOG_SIZE_BYTES, backupCount=BACKUP_COUNT)
+log_handler.setFormatter(log_formatter)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(log_handler)
 
 def get_fs_usage(path):
     """Get filesystem usage percentage."""
@@ -38,9 +49,9 @@ def move_file(src, dest):
     """Move a file and log the action."""
     try:
         shutil.move(src, dest)
-        logging.info(f"Moved {src} to {dest}")
+        logger.info(f"Moved {src} to {dest}")
     except Exception as e:
-        logging.error(f"Error moving file: {e}")
+        logger.error(f"Error moving file: {e}")
 
 def move_files_concurrently(files_to_move):
     with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
@@ -49,12 +60,12 @@ def move_files_concurrently(files_to_move):
             try:
                 future.result()
             except Exception as e:
-                logging.error(f"Error moving file: {e}")
+                logger.error(f"Error moving file: {e}")
 
 def main():
     current_usage = get_fs_usage(CACHE_PATH)
     if current_usage > THRESHOLD_PERCENTAGE:
-        logging.info(f"Cache usage is {current_usage:.2f}%, exceeding threshold. Starting file move...")
+        logger.info(f"Cache usage is {current_usage:.2f}%, exceeding threshold. Starting file move...")
         files_to_move = gather_files_to_move()
         move_files_concurrently(files_to_move)
 
