aad1c23b9cf5cf79706ff15b6ea7c028aa5891f2
muffin <MonsterMuffin@users.noreply.github.com>
2023-10-17 14:28:39 +0100

    reworked (again)

--- cache-mover.py
+++ cache-mover.py
@@ -29,63 +29,74 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logger.addHandler(log_handler)
 
-def is_script_running():
-    try:
-        # Use ps and grep to count instances of the script running
-        cmd = f"ps aux | grep {os.path.basename(__file__)} | grep -v grep | wc -l"
-        count = int(subprocess.check_output(cmd, shell=True).strip())
-
-        # If count > 1, then another instance of the script is running
-        # (The current instance is also counted.)
-        return count > 1
-    except Exception as e:
-        logging.error(f"Error checking for running script instances: {e}")
-        return False
-
 def get_fs_usage(path):
-    """Get filesystem usage percentage."""
     total, used, free = shutil.disk_usage(path)
     return (used / total) * 100
 
 def gather_files_to_move():
-    """Gather files to be moved until the threshold is no longer exceeded."""
+    all_files = [
+        os.path.join(dirname, filename)
+        for dirname, _, filenames in os.walk(CACHE_PATH)
+        for filename in filenames
+    ]
+
+    if not all_files:
+        logging.warning("No files found in CACHE_PATH.")
+        return []
+
+    all_files.sort(key=lambda fn: os.stat(fn).st_mtime)
     files_to_move = []
-    while get_fs_usage(CACHE_PATH) > THRESHOLD_PERCENTAGE:
-        oldest_file = min(
-            (os.path.join(dirname, filename)
-            for dirname, _, filenames in os.walk(CACHE_PATH)
-            for filename in filenames),
-            key=lambda fn: os.stat(fn).st_mtime)
+
+    while get_fs_usage(CACHE_PATH) > THRESHOLD_PERCENTAGE and all_files:
+        oldest_file = all_files.pop(0)
         files_to_move.append(oldest_file)
     return files_to_move
 
 def move_file(src, dest):
-    """Move a file and log the action."""
+    # Move a file using rsync and log the action.
     try:
-        shutil.move(src, dest)
+        cmd = ["rsync", "-avh", "--remove-source-files", src, dest]
+        subprocess.check_call(cmd)
         logger.info(f"Moved {src} to {dest}")
+    except subprocess.CalledProcessError:
+        logger.error(f"Error moving file using rsync.")
     except Exception as e:
-        logger.error(f"Error moving file: {e}")
+        logger.error(f"Unexpected error moving file: {e}")
 
 def move_files_concurrently(files_to_move):
     with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-        futures = [executor.submit(move_file, src, os.path.join(BACKING_PATH, os.path.basename(src))) for src in files_to_move]
+        futures = [executor.submit(move_file, src, BACKING_PATH) for src in files_to_move]
         for future in futures:
             try:
                 future.result()
             except Exception as e:
                 logger.error(f"Error moving file: {e}")
 
-def main():
-    if is_script_running():
-        logging.warning("Another instance of the script is running. Exiting.")
+def delete_empty_dirs(path):
+    """Recursively delete empty directories."""
+    # If the path itself is not a directory, exit.
+    if not os.path.isdir(path):
         return
 
+    # Remove child directories first.
+    for child_dir in [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]:
+        delete_empty_dirs(os.path.join(path, child_dir))
+
+    # Check if the directory is empty.
+    if not os.listdir(path) and path not in [os.path.join(CACHE_PATH, d) for d in os.listdir(CACHE_PATH) if os.path.isdir(os.path.join(CACHE_PATH, d))]:
+        logging.info(f"Removed empty directory: {path}")
+        os.rmdir(path)
+
+def main():
     current_usage = get_fs_usage(CACHE_PATH)
     if current_usage > THRESHOLD_PERCENTAGE:
         logging.info(f"Cache usage is {current_usage:.2f}%, exceeding threshold. Starting file move...")
         files_to_move = gather_files_to_move()
         move_files_concurrently(files_to_move)
+
+    # Clean up any empty directories under the cache path
+    for root_folder in [os.path.join(CACHE_PATH, d) for d in os.listdir(CACHE_PATH) if os.path.isdir(os.path.join(CACHE_PATH, d))]:
+        delete_empty_dirs(root_folder)
 
 if __name__ == "__main__":
     main()
